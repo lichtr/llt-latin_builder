151b19045fd27b846914feaeaed181877a41119f
lichtr <robert.lichtensteiner@gmail.com>
2014-02-10 17:22:31 +0100

    Adds random numerus for predicate and subject.

--- lib/llt/latin_builder/sentence_creator.rb
+++ lib/llt/latin_builder/sentence_creator.rb
@@ -7,9 +7,9 @@ module LLT
         @obj = opts[:obj]
         @pred = opts[:pred]
 
-        @sbj_opts = { numerus: 1, casus: 1 }
+        @pred_opts = { persona: 3, numerus: random_numerus, tempus: :praesens, genus: :activum, modus: :indicativus }
+        @sbj_opts = { numerus: adapt_numerus_to_pred, casus: 1 }
         @obj_opts  = { numerus: 1, casus: 4 }
-        @pred_opts = { persona: 3, numerus: 1, tempus: :praesens, genus: :activum, modus: :indicativus }
 
         @sbj_args  = { type: :noun, stem: @sbj.stem, nominative: @sbj.nom, inflection_class: @sbj.inflectable_class, sexus: @sbj.sexus, options: @sbj_opts }
         @pred_args = { type: :praesens, stem: @pred.pr_stem, inflection_class: @pred.inflectable_class, options: @pred_opts }
@@ -22,6 +22,15 @@ module LLT
         end.flatten
       end
 
+      private
+
+      def random_numerus
+        rand(1..2)
+      end
+
+      def adapt_numerus_to_pred
+        @pred_opts[:numerus]
+      end
     end
   end
 end
